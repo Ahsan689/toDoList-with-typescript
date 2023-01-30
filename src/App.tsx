f1e21e0fb4f7dcf2aab73c30b116cadf2cde71ff
ahsan689 <ahsanaleem689@gmail.com>
2023-01-31 00:57:55 +0500

    our first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [Data_State, setData_State] = useState<To_Do[]>([])
+  const [item, setitem] = useState('')
+  interface To_Do {
+    id:string,
+    item:string
+  }
+  let date = new Date()
+  let Data: To_Do[] = []
+
+  
+  function add_Task(id:string){
+    
+    // Data.push({id:id,item:item})
+    // console.log(Data);
+    setData_State([...Data_State,{id:id,item:item}])
+  }
+  console.log(Data_State,"DATa");
+  // add_Task()
+  
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          style={{cursor:'pointer'}}
+          // href=""
+          // target="_blank"
+          // rel="noopener noreferrer"
+          
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <input onChange={e => setitem(e.target.value)}/>
+      <button onClick={()=> {add_Task(date.getTime().toString())}}> add task </button>
     </div>
   );
 }
